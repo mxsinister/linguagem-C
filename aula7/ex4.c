366cbe707edc0d7017db015a08ca6fcd6d3758ab
mxsinister <93889237+mxsinister@users.noreply.github.com>
2025-05-30 16:13:09 -0300

    finalizada

--- aula7/ex4.c
+++ aula7/ex4.c
@@ -4,28 +4,36 @@
 int main() {
 
     float notas[10];
-    float soma = 0;
     float media = 0;
     float nota;
+    int acima = 0;
 
     for (int i = 0; i < 10; i++) {
         printf("digite a nota do aluno %d: ", i + 1);
         scanf("%f", &nota);
 
-        notas[i] = nota
-        ;
+        notas[i] = nota;
+        media += notas[i];
+        
     }
+    media = media / 10;
 
     for (int i = 0; i < 10; i++) {
-        soma += notas[i];
+        if (notas[i] > media){
+            acima++;
+        }
     }
-    media = soma / 10;
+    printf ("Media das notas: %.2f\n", media);
+    printf("Quantidade de notas acima da media: %d\n", acima);
+    printf("Notas acima da media:");
 
     for (int i = 0; i < 10; i++) {
-        printf("Nota do aluno %d: %.2f\n", i+1, notas[i]);
-        if notas[i] > acima
+        if (notas[i] > media){
+            printf("\n- %.2f", notas[i]);
+        }
     }
-    printf ("Media das notas = %.2f", media);
+
+
 
 }
 
