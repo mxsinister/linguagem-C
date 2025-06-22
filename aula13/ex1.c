c5dfa215b39febb8e2e32de5678133cd2c688e76
mxsinister <93889237+mxsinister@users.noreply.github.com>
2025-06-22 19:23:00 -0300

    Update ex1.c

--- aula13/ex1.c
+++ aula13/ex1.c
@@ -4,7 +4,7 @@ int main(){
     char c;
     int caracteres = 0;
 
-    FILE *arquivo = fopen("../arquivo.txt", "r");
+    FILE *arquivo = fopen("./arquivo.txt", "r");
 
     if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo!\n");
